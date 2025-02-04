e57d3834e631983c1eef69dae3c5940c3f38ccc4
Vishal <145148874+vishal7bhalke@users.noreply.github.com>
2025-02-04 16:55:15 +0530

    Update Footer.jsx

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="my-10 py-4 text-center text-gray-400">
       <h2 className="text-lg font-medium">
-        Created by Tushar Tiwari | Ai Travel Planner App | © {new Date().getFullYear()}
+        Created by Vishal | Ai Travel Planner App | © {new Date().getFullYear()}
       </h2>
     </footer>
   );
